cfc134c911d9c9a2bddcad2963ec15180dcf9ea5
Ethanyan <yanyuliang6@163.com>
2019-05-23 15:59:25 +0800

    添加测试函数，显示hello world

---
+++ boards/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from boards import views
+
+
+urlpatterns = [
+    url(r'^home/$',views.home),
+]
--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 添加子应用
+    'boards',
 ]
 
 MIDDLEWARE = [
