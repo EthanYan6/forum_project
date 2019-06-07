b09eb810375a011af117a18265d176a24222dc8f
EthanYan6 <yanyuliang6@163.com>
2019-06-07 22:56:21 +0800

    继续添加表单测试

--- boards/tests.py
+++ boards/tests.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from django.urls import reverse
 from django.test import TestCase
 from django.urls import resolve
 from boards.views import home, board_topics, new_topic
-from boards.models import Board
+from boards.models import Board, Topic, Post
 
 
 class HomeTest(TestCase):
@@ -36,6 +37,7 @@ class BoardTopicsTests(TestCase):
     def setUp(self):
         # 此处需要填写id,否则后面单元测试无法通过,结果未知
         Board.objects.create(id=1, name='Django', description='Django board.')
+        User.objects.create(username='john', email='john@doe.com', password='123') # <- indluded this line here
 
 
     def test_board_topics_view_success_status_code(self):
@@ -46,7 +48,7 @@ class BoardTopicsTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-    def test_board_toopics_view_not_found_status_code(self):
+    def test_board_topics_view_not_found_status_code(self):
         # 测试Django是否对于不存在于数据库的Board返回status code 404(页面未找到)
         url = reverse('board_topics', kwargs={'pk': 99})
         # url = reverse('bt: board_topics', kwargs={'pk': 99})
@@ -57,6 +59,51 @@ class BoardTopicsTests(TestCase):
         # 测试Django是否使用了正确的视图函数去渲染topics
         view = resolve('/boards/1/')
         self.assertEqual(view.func, board_topics)
+
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        """
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        :return:
+        """
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        """
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        :return:
+        """
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(Topic.objects.exists())
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
 
 
 class NewTopicTests(TestCase):
