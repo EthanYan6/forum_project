d2d5875764383660b923e315fc2904b2ab87a793
EthanYan6 <yanyuliang6@163.com>
2019-06-07 23:18:08 +0800

    修复测试类

--- boards/tests.py
+++ boards/tests.py
@@ -16,11 +16,11 @@ class HomeTest(TestCase):
         url = reverse('home')
         # url = reverse('boards:home')
         response = self.client.get(url)
-        self.assertEqual(response.status_code,200)
+        self.assertEquals(response.status_code,200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEqual(view.func, home)
+        self.assertEquals(view.func, home)
 
     def test_home_view_contains_link_to_topics_page(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
@@ -46,19 +46,19 @@ class BoardTopicsTests(TestCase):
         # print(url)
         # url = reverse('bt: board_topics', kwargs={'pk': 1})
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEquals(response.status_code, 200)
 
     def test_board_topics_view_not_found_status_code(self):
         # 测试Django是否对于不存在于数据库的Board返回status code 404(页面未找到)
         url = reverse('board_topics', kwargs={'pk': 99})
         # url = reverse('bt: board_topics', kwargs={'pk': 99})
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEquals(response.status_code, 404)
 
     def test_board_topics_url_resolves_board_topics_view(self):
         # 测试Django是否使用了正确的视图函数去渲染topics
         view = resolve('/boards/1/')
-        self.assertEqual(view.func, board_topics)
+        self.assertEquals(view.func, board_topics)
 
     def test_board_topics_view_contains_navigation_links(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
@@ -101,7 +101,6 @@ class BoardTopicsTests(TestCase):
         }
         response = self.client.post(url, data)
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(Topic.objects.exists())
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
 
@@ -113,16 +112,16 @@ class NewTopicTests(TestCase):
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEquals(response.status_code, 200)
 
     def test_new_topic_view_not_found_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 99})
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEquals(response.status_code, 404)
 
     def test_new_topic_url_resolves_new_topic_view(self):
         view = resolve('/boards/1/new/')
-        self.assertEqual(view.func, new_topic)
+        self.assertEquals(view.func, new_topic)
 
     def test_new_topic_view_contains_link_back_to_board_topics_view(self):
         new_topic_url = reverse('new_topic', kwargs={'pk': 1})
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
+
+from boards.forms import NewTopicForm
 from .models import Board, Topic, Post
 
 
@@ -22,25 +24,42 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first() # TODO: get the currently logged in user
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        # subject = request.POST['subject']
+        # message = request.POST['message']
+        #
+        # user = User.objects.first() # TODO: 临时使用一个账号作为登录用户
+        #
+        # topic = Topic.objects.create(
+        #     subject=subject,
+        #     board=board,
+        #     starter=user
+        # )
+        #
+        # post = Post.objects.create(
+        #     message=message,
+        #     topic=topic,
+        #     created_by=user
+        # )
+        #
+        # return redirect('board_topics', pk=board.pk) # TODO:redirect to the created topic page
 
-        user = User.objects.first() # TODO: 临时使用一个账号作为登录用户
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk) # TODO:redirect to the created topic page
     return render(request, 'new_topic.html', {'board': board})
 
