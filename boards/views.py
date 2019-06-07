6fd4330dcef3dc2eed6114b17556132984cb1c4a
EthanYan6 <yanyuliang6@163.com>
2019-06-07 21:30:48 +0800

    前端添加表单

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from .models import Board
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -21,5 +22,25 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first() # TODO: 临时使用一个账号作为登录用户
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk) # TODO:redirect to the created topic page
     return render(request, 'new_topic.html', {'board': board})
 
