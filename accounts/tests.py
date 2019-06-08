86701f48922b761f8c243bfbc0f1ea884a660adf
EthanYan6 <yanyuliang6@163.com>
2019-06-08 14:46:32 +0800

    添加email字段,并添加测试代码

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.urls import resolve
+
+from accounts.forms import SignUpForm
 from .views import signup
 
 
@@ -24,7 +26,19 @@ class SignUpTest(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        """
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        :return:
+        """
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
 
 
 class SuccessfulSignUpTests(TestCase):
@@ -32,6 +46,7 @@ class SuccessfulSignUpTests(TestCase):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
+from .forms import SignUpForm
+
 
 # Create your views here.
 def signup(request):
@@ -12,5 +14,5 @@ def signup(request):
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
