d72e6e62c2f429c7726426866d418260d20be021
EthanYan6 <yanyuliang6@163.com>
2019-06-09 14:08:36 +0800

    添加用户注销功能

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,3 +135,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from boards import views
 from django.contrib import admin
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
     # url(r'^$', include('boards.urls', namespace='boards')),
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     # url(r'^', include('boards.urls', namespace='bt')),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
