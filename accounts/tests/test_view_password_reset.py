7e5b9ef9a093a882a9f4e793c854c28f558eccd6
EthanYan6 <yanyuliang6@163.com>
2019-06-30 15:23:33 +0800

    密码重置完成测试用例

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -69,3 +69,16 @@ class InvalidPasswordResetTests(TestCase):
 
     def test_no_reset_email_sent(self):
         self.assertEqual(0, len(mail.outbox))
+
+
+class PasswordRestDoneTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_done')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/done/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
