07915ef3f882c4beb67d27db498b7a871afb2cdc
EthanYan6 <yanyuliang6@163.com>
2019-07-18 17:51:41 +0800

    添加密码修改页面以及密码修改成功页面

--- myproject/urls.py
+++ myproject/urls.py
@@ -55,5 +55,7 @@ urlpatterns = [
             template_name='password_reset_complete.html'
         ),
         name='password_reset_complete'),
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),name='password_change_done'),
 
 ]
