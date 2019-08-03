2e5e7824a8f03faf8f6c8ac2f30f78d2a08c6c0d
EthanYan6 <yanyuliang6@163.com>
2019-08-03 15:19:06 +0800

    将发布主题的用户设置为当前登录的用户

--- boards/views.py
+++ boards/views.py
@@ -25,7 +25,6 @@ def board_topics(request, pk):
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first() # TODO: get the currently logged in user
 
     if request.method == 'POST':
         # subject = request.POST['subject']
@@ -51,12 +50,12 @@ def new_topic(request, pk):
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
-            post = Post.objects.create(
+            Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
     else:
