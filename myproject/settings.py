b87cde18debca209dfc8836d072939529ab8a01f
EthanYan6 <yanyuliang6@163.com>
2019-10-04 19:52:12 +0800

    使用图片gravatar

--- myproject/settings.py
+++ myproject/settings.py
@@ -72,6 +72,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'gravatar': 'templates.gravatar',
+
+            },
+
         },
     },
 ]
