50d064a2b7b4316a53fe9339e91b8365a6d6fdf8
EthanYan6 <yanyuliang6@163.com>
2019-10-04 17:11:52 +0800

    编辑帖子页面

--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.views.generic import UpdateView
+from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from boards.forms import NewTopicForm
 from .models import Board, Topic, Post
 from django.db.models import Count
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 def home(request):
@@ -84,3 +87,22 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit = False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
--- myproject/urls.py
+++ myproject/urls.py
@@ -59,5 +59,6 @@ urlpatterns = [
     url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),name='password_change_done'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',views.PostUpdateView.as_view(), name='edit_post')
 
 ]
